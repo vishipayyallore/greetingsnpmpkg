eea753d0b69cf42bdb296c7601fd2c07e1f6b9fe
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-01-20 23:16:08 +0530

    GitHub Work Flow

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const greetUser = function (name) {
+    const message = `${new Date().toUTCString()} :: Hello, ${name}`;
+
+    console.log(message);
+
+    return message;
+}
+
+module.exports = { greetUser };
